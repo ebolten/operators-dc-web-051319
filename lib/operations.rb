9aa2f595dd696138a70d8fd1d3abab98c9584f54
Emily Bolten <emilybolten@gmail.com>
2019-04-29 15:20:29 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,11 +11,6 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60
-		return true
-	elsif speed < 40
-		return true
-	else
-		return false
-	end
+	return true ? speed > 60 : return false
+	return true ? speed < 40 : return false
 end
